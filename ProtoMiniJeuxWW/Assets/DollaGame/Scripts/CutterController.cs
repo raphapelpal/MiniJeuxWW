278ac5e17701a49ae8be40235c2c5dcef8b9f4cc
Raphael <r.pelletierpalumbo@rubika-edu.com>
2021-12-10 10:56:47 +0100

    Dernières modifs Dolla game
    Avant le début de l'intégration au macro jeu

--- ProtoMiniJeuxWW/Assets/DollaGame/Scripts/CutterController.cs
+++ ProtoMiniJeuxWW/Assets/DollaGame/Scripts/CutterController.cs
@@ -2,24 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class CutterController : MonoBehaviour
 {
     private Vector2 joystickInput;
     [SerializeField] private float speed = 5f;
-    public bool isLosingQuality = false;
+    public bool isLosingQuality = false, hasFinishedGame;
     [SerializeField] private int cutsLeft = 39;
-    [SerializeField] private GameObject winScreen;
-    public float quality = 10;
+    public float currentQuality, maxQuality;
     private SpriteRenderer cutterSprite;
     private CircleCollider2D cutterCollider;
-    [SerializeField] private GameObject contoursEasy, contoursMedium, contoursHard;
+    [SerializeField] private GameObject contoursEasy, contoursMedium, contoursHard, winScreen, loseScreen, canvasObj;
     public bool isEasy, isMedium, isHard;
+    [SerializeField] private RectTransform qualityBarTransform;
+    [SerializeField] private Image qualityBarImage;
+    [SerializeField] private Color goodQualityColor, badQualityColor;
 
     private void Start()
     {
         cutterSprite = GetComponent<SpriteRenderer>();
         cutterCollider = GetComponent<CircleCollider2D>();
+        currentQuality = maxQuality;
+        hasFinishedGame = false;
         
         if (isEasy)
         {
@@ -40,24 +45,38 @@ public class CutterController : MonoBehaviour
 
     void FixedUpdate()
     {
-        joystickInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
-        transform.position += new Vector3(joystickInput.x, -joystickInput.y, 0) * speed * Time.fixedDeltaTime;
-
-        if (isLosingQuality)
+        if (!hasFinishedGame)
         {
-            quality -= Time.deltaTime*6;
+            //Joystick Input
+            joystickInput = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            transform.position += new Vector3(joystickInput.x, -joystickInput.y, 0) * speed * Time.fixedDeltaTime;
+        
+            //Quality Bar Feedback
+            qualityBarTransform.localScale = new Vector2(currentQuality / maxQuality * 15, 1f);
+            qualityBarImage.color = Color.Lerp( badQualityColor, goodQualityColor, currentQuality / maxQuality);
+
+            if (isLosingQuality)
+            {
+                currentQuality -= Time.deltaTime*6;
+            }
         }
 
         if (cutsLeft == 0)
         {
+            canvasObj.SetActive(false);
             winScreen.SetActive(true);
             cutterSprite.enabled = false;
             cutterCollider.enabled = false;
+            hasFinishedGame = true;
         }
 
-        if (quality < 0)
+        if (currentQuality < 0)
         {
-            //Lose
+            canvasObj.SetActive(false);
+            loseScreen.SetActive(true);
+            cutterSprite.enabled = false;
+            cutterCollider.enabled = false;
+            hasFinishedGame = true;
         }
     }
 
