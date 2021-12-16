9a859594171d159432cea9e7ace959e926b4a4d2
Raphael <r.pelletierpalumbo@rubika-edu.com>
2021-12-16 10:59:11 +0100

    Correction des noms des sons

--- WarioTemplate/Assets/MicroGames/MG_ULC_DollaGame/Scripts/CutterController.cs
+++ WarioTemplate/Assets/MicroGames/MG_ULC_DollaGame/Scripts/CutterController.cs
@@ -71,7 +71,7 @@ public class CutterController : MonoBehaviour, ITickable
                 StartCoroutine(HolupALilBit(3f));
             }
         }
-        
+
         //Bill Cut Check
         if (cornersReached == 4)
         {
