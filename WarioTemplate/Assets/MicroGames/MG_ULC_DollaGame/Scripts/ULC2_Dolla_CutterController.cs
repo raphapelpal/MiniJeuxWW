ef12f498f649b2daa67ce67f65cb7676d3a4b44d
raphapelpal <r.pelletierpalumbo@rubika-edu.com>
2022-01-24 23:08:33 +0000

    DollaGame difficulty 3 correction

--- WarioTemplate/Assets/MicroGames/MG_ULC_DollaGame/Scripts/ULC2_Dolla_CutterController.cs
+++ WarioTemplate/Assets/MicroGames/MG_ULC_DollaGame/Scripts/ULC2_Dolla_CutterController.cs
@@ -40,7 +40,7 @@ public class ULC2_Dolla_CutterController : MonoBehaviour, ITickable
         else if(GameController. difficulty == 3)
         {
             billsToCut = 2;
-            speed = speed * 1.75f;
+            speed = speed * 1.5f;
             ActivateBills(2);
         }
     }
